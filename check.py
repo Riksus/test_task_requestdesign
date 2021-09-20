8c17388f2034765ce80b2e24f7a950529ae7f4e8
riksus <riksus1@gmail.com>
2021-09-21 00:57:53 +0300

    feat:add status_check function

--- check.py
+++ check.py
@@ -1,4 +1,4 @@
-
+import urllib.request
 def read_checklist(checklisfile):
     with open(checklisfile, "r") as ins:
         array = []
@@ -6,5 +6,13 @@ def read_checklist(checklisfile):
             line=line.rstrip()
             array.append(line)
         return array
+
+def status_check(adress):
+    adress = "http://" + adress
+    status = urllib.request.urlopen(adress).getcode()
+    if status == "200":
+        return adress
+    else:
+        return 0
 
 iplist=read_checklist("checklist.txt")
