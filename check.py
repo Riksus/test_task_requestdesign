2e33d9b4fabd33c7f0ba6abd5e28b590d6d49fc4
Riksus <riksus1@gmail.com>
2021-09-23 06:11:58 +0300

    feat:add exception for func and first logic

--- check.py
+++ check.py
@@ -1,5 +1,8 @@
-import urllib.request
 import requests
+import time
+from datetime import datetime
+#функции
+
 def read_checklist(checklisfile):
     with open(checklisfile, "r") as ins:
         array = []
@@ -10,17 +13,50 @@ def read_checklist(checklisfile):
 
 def status_check(adress):
     adress = "http://" + adress
-    status = urllib.request.urlopen(adress).getcode()
-    if status == "200":
-        return adress
+    status = 0
+    try: 
+        status = requests.get(adress).status_code
+    except requests.exceptions.HTTPError as errh:
+        print("Http Error:",adress,errh,now, file=log)
+    except requests.exceptions.ConnectionError as errc:
+        print("Error Connecting:",adress,errc,now, file=log,)
+    except requests.exceptions.Timeout as errt:
+        print("Timeout Error:",adress,errt,now, file=log)
+    except requests.exceptions.RequestException as err:
+        print("OOps: Something Else",adress,err,now, file=log)
+    if status == 200:
+        return 1
     else:
         return 0
 
 def sendfail(adress,sendlist):
     failmess = {"failed" : adress}
     for ip in sendlist:
-        requests.post(ip, data=failmess)
-    return
+        try: 
+            requests.post(ip, data=failmess)
+        except requests.exceptions.HTTPError as errh:
+            print("Http Error:",adress,errh,now, file=log)
+        except requests.exceptions.ConnectionError as errc:
+            print("Error Connecting:",adress,errc,now, file=log,)
+        except requests.exceptions.Timeout as errt:
+            print("Timeout Error:",adress,errt,now, file=log)
+        except requests.exceptions.RequestException as err:
+            print("OOps: Something Else",adress,err,now, file=log)
+    return print (requests.post(ip, data=failmess), ip)
 
+#функции
+
+#логика
 ipchecklist=read_checklist("checklist.txt")
 ipsendlist=read_checklist("sendlist.txt")
+log=open('log.txt','a')
+now = datetime.now()
+while True:
+    for ip in ipchecklist:
+        print(ip) 
+        if status_check(ip) == 0:
+            print("недоступен",ip)
+            sendfail(ip,ipsendlist)
+    print("сделал")
+    time.sleep(60)
+    print("минута")
